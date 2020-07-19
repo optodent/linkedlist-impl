19d4445409a2d33154b02b32e86e97a63a0a54f6
unknown <test@test.test>
2020-07-19 17:42:48 +0300

    add examples in main method.

--- src/io/rusev/Main.java
+++ src/io/rusev/Main.java
@@ -2,11 +2,7 @@ package io.rusev;
 
 import io.rusev.collections.LinkedList;
 import io.rusev.collections.List;
-import io.rusev.collections.Node;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Iterator;
 import java.util.stream.IntStream;
 
 public class Main {
@@ -16,7 +12,15 @@ public class Main {
         List<Integer> list = new LinkedList<>();
 
         IntStream.range(0, 100).forEach(e -> list.add(e));
-        System.out.println(list.remove(Integer.valueOf(1)));
+        list.add(-1, 0);
+
+        System.out.println(list.contains(0));
+        System.out.println(list.remove(0));
+        System.out.println(list.remove(Integer.valueOf(0)));
+        System.out.println(list.getElement(0));
+        System.out.println(list.isEmpty());
+        System.out.println(list.size());
+
         System.out.println(list);
     }
 }
